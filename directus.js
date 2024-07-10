c68ddddb9d80611a508df5f9acd262cb889770a7
Denperidge <denperidge@gmail.com>
2024-07-10 14:23:09 +0200

    Updated directus.js, added text heading for media

--- directus.js
+++ directus.js
@@ -5,13 +5,14 @@ const { createDirectus, rest, readItems, staticToken } = require("@directus/sdk"
 function _handleError(err) { if (err) { console.error(err); };}
 
 module.exports = async function({cmsUrl="", staticTokenValue="", collectionName="", outputFilename="src/_data/{{collectionName}}.json", configFilename=undefined}) {
+    let config = {}
     if (configFilename) {
-        const config = JSON.parse(readFileSync(configFilename, {encoding: "utf-8"}));
-        cmsUrl = cmsUrl || config["cmsUrl"] || env.CMS_URL;
-        staticTokenValue = staticTokenValue || config["staticTokenValue"] || config["staticToken"] || env.STATIC_TOKEN;
-        collectionName = collectionName || config["collectionName"] || env.COLLECTION_NAME;
-        outputFilename = outputFilename || config["outputFilename"]  || env.OUTPUT_FILENAME;
+        config = JSON.parse(readFileSync(configFilename, {encoding: "utf-8"}));
     }
+    cmsUrl = cmsUrl || config["cmsUrl"] || env.CMS_URL;
+    staticTokenValue = staticTokenValue || config["staticTokenValue"] || config["staticToken"] || env.STATIC_TOKEN;
+    collectionName = collectionName || config["collectionName"] || env.COLLECTION_NAME;
+    outputFilename = outputFilename || config["outputFilename"]  || env.OUTPUT_FILENAME;
 
     outputFilename = outputFilename.replace("{{collectionName}}", collectionName);
 
